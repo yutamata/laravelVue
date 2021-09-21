7248e83d31e7939f47ee40d1d5cd7444259f2322
yutamata <game.world.brain@gmail.com>
2021-09-22 02:29:18 +0900

    編集画面とAPIつなぎ込み

--- my-app/resources/js/components/TaskEditComponent.vue
+++ my-app/resources/js/components/TaskEditComponent.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-sm-6">
-        <form>
+        <form v-on:submit.prevent="submit">
           <div class="form-group row">
             <label for="id" class="col-sm-3 col-form-label">ID</label>
             <input
@@ -10,16 +10,26 @@
               class="col-sm-9 form-control-plaintext"
               readonly
               id="id"
-              v-bind:value="taskId"
+              v-model="task.id"
             />
           </div>
           <div class="form-group row">
             <label for="title" class="col-sm-3 col-form-label">Title</label>
-            <input type="text" class="col-sm-9 form-control" id="title" />
+            <input
+              type="text"
+              class="col-sm-9 form-control"
+              id="title"
+              v-model="task.title"
+            />
           </div>
           <div class="form-group row">
             <label for="content" class="col-sm-3 col-form-label">Content</label>
-            <input type="text" class="col-sm-9 form-control" id="content" />
+            <input
+              type="text"
+              class="col-sm-9 form-control"
+              id="content"
+              v-model="task.content"
+            />
           </div>
           <div class="form-group row">
             <label for="person-in-charge" class="col-sm-3 col-form-label"
@@ -29,6 +39,7 @@
               type="text"
               class="col-sm-9 form-control"
               id="person-in-charge"
+              v-model="task.person_in_charge"
             />
           </div>
           <button type="submit" class="btn btn-primary">Submit</button>
@@ -38,10 +49,31 @@
   </div>
 </template>
  
- <script>
+
+<script>
 export default {
   props: {
     taskId: String,
+  },
+  data: function () {
+    return {
+      task: {},
+    };
+  },
+  methods: {
+    getTask() {
+      axios.get("/api/tasks/" + this.taskId).then((res) => {
+        this.task = res.data;
+      });
+    },
+    submit() {
+      axios.put("/api/tasks/" + this.taskId, this.task).then((res) => {
+        this.$router.push({ name: "task.list" });
+      });
+    },
+  },
+  mounted() {
+    this.getTask();
   },
 };
 </script>
