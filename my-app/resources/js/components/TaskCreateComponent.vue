86917547336795b7e2b1b37d1d18b0327849361d
yutamata <game.world.brain@gmail.com>
2021-09-22 02:23:34 +0900

    作成APIと画面

--- my-app/resources/js/components/TaskCreateComponent.vue
+++ my-app/resources/js/components/TaskCreateComponent.vue
@@ -2,14 +2,24 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-sm-6">
-        <form>
+        <form v-on:submit.prevent="submit">
           <div class="form-group row">
             <label for="title" class="col-sm-3 col-form-label">Title</label>
-            <input type="text" class="col-sm-9 form-control" id="title" />
+            <input
+              type="text"
+              class="col-sm-9 form-control"
+              id="title"
+              v-model="task.title"
+            />
           </div>
           <div class="form-group row">
             <label for="content" class="col-sm-3 col-form-label">Content</label>
-            <input type="text" class="col-sm-9 form-control" id="content" />
+            <input
+              type="text"
+              class="col-sm-9 form-control"
+              id="content"
+              v-model="task.content"
+            />
           </div>
           <div class="form-group row">
             <label for="person-in-charge" class="col-sm-3 col-form-label"
@@ -19,6 +29,7 @@
               type="text"
               class="col-sm-9 form-control"
               id="person-in-charge"
+              v-model="task.person_in_charge"
             />
           </div>
           <button type="submit" class="btn btn-primary">Submit</button>
@@ -28,6 +39,19 @@
   </div>
 </template>
  
- <script>
-    export default {};
+<script>
+export default {
+  data: function () {
+    return {
+      task: {},
+    };
+  },
+  methods: {
+    submit() {
+      axios.post("/api/tasks", this.task).then((res) => {
+        this.$router.push({ name: "task.list" });
+      });
+    },
+  },
+};
 </script>
